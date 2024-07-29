19f32a38875eeee11385cc54ea6de932ff36308d
Andy Nzeka <43660682+andynze1@users.noreply.github.com>
2024-07-29 16:36:59 -0500

    Update eks.tf

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -24,6 +24,11 @@ resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
   role       = aws_iam_role.eks_cluster.name
 }
 
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster.name
+}
+
 # Create EKS Cluster
 resource "aws_eks_cluster" "vtech-cluster" {
   name     = "${terraform.workspace}-vtech-eks-cluster"
