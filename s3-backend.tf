456f120db8ab2b29dfa167e2b6777497823c301c
Andy Nzeka <andynze4@gmail.com>
2025-01-24 20:25:00 -0600

    Working code

--- s3-backend.tf
+++ s3-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state4"
+    bucket = "my-terraform-state41"
     key    = "environments2"
     region = "us-east-1"
     #    dynamodb_table = "ndcc-terraform-state-lock-dynamo"
